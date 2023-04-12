fbc5ff56ceedcd9cfa73a2bf4251c03676f00cbe
Andrew Kelley <andrew@ziglang.org>
2023-04-12 09:57:30 -0700

    update to latest build.zig API

--- build.zig
+++ build.zig
@@ -25,9 +25,9 @@ pub fn build(b: *std.build.Builder) void {
     exe.linkLibrary(libespresso.artifact("espresso"));
     exe.linkLibrary(libeqntott.artifact("eqntott-lib"));
     exe.linkLibC();
-    exe.install();
+    b.installArtifact(exe);
 
-    const run_cmd = exe.run();
+    const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
     if (b.args) |args| {
         run_cmd.addArgs(args);
